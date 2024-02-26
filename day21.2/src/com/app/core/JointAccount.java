d8dc44d78d662d1d7ea08cec9d5426f5ef8b056e
AnkitLipate <130392155+AnkitLipate@users.noreply.github.com>
2024-02-26 09:47:55 +0530

    Add files via upload

---
+++ day21.2/src/com/app/core/JointAccount.java
@@ -0,0 +1,34 @@
+package com.app.core;
+
+import static java.lang.Thread.currentThread;
+
+public class JointAccount {
+	private double balance;
+
+	public JointAccount(double balance) {
+		super();
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "JointAccount [balance=" + balance + "]";
+	}
+
+	// update balance : orig bal = 5000, amount=1000
+	public synchronized void updateBalance(double amt) throws Exception {
+		System.out.println("updating balance : " + currentThread().getName());
+		balance = balance + amt;//6000
+		System.out.println("Cancelling updates, after a dly");
+		Thread.sleep(20);// think time
+		balance = balance - amt;//5000
+		System.out.println("Updation over by " + currentThread().getName());
+	}// bal = 5000	
+
+	// check balance
+	public synchronized double checkBalance() throws Exception {
+		System.out.println("Checking balance by " + currentThread().getName());
+	//	Thread.sleep(37);
+		return balance;
+	}//bal = 5000
+}
